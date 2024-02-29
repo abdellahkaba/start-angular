42307a197f56266df23eff93ee0bc4cd4a20d8dc
Abdellah <abdallahkaba98@gmail.com>
2024-02-29 06:12:26 +0000

    navigation-routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ProductsComponent} from "./products/products.component";
+import {CustomersComponent} from "./customers/customers.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Declaration de routes
+  {path : "products", component : ProductsComponent},
+  {path : "customers", component : CustomersComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
