250d4674ee8d2c494d730717d1a0773d315a34b7
Abdellah <abdallahkaba98@gmail.com>
2024-02-29 14:41:09 +0000

    Authentification1

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import {AppUser} from "../model/user.mode";
+import {UUID} from "angular2-uuid";
+import {Observable, of, throwError} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  users : AppUser[] = []
+  authenticatedUser : AppUser | undefined
+
+  constructor() {
+    this.users.push({userId : UUID.UUID(), username : "Mariam", password : "1234", roles : ["USERS"]})
+    this.users.push({userId : UUID.UUID(), username : "Oumar", password : "1234", roles : ["USERS"]})
+    this.users.push({userId : UUID.UUID(), username : "Abdellah", password : "1234", roles : ["USERS","ADMIN"]})
+  }
+
+  //une methode qui fait auth
+  public login (username : string, password : string) : Observable<AppUser>{
+    //On cherche username
+   let appUser = this.users.find(u => u.username == username)
+    if(!appUser) return  throwError(() => new Error("User not found"))
+    if(appUser.password!=password){
+      return  throwError(() => new Error("Band credential"))
+    }
+
+    return of(appUser)
+  }
+
+  //Service authentification
+  public authenticateUser(appUser : AppUser) : Observable<boolean>{
+    this.authenticatedUser = appUser
+    //on le stocke dans le local storage
+    localStorage.setItem("authUser", JSON.stringify({username : appUser.username, roles :appUser.roles, jwt : "JWT_TOKEN" }))
+    return of(true)
+  }
+
+  //une methode qui verifie si cest admin ou user
+  public hasRole (role : string) : boolean {
+    //voir si c'est un admin
+  return this.authenticatedUser!.roles.includes(role)
+
+  }
+  //Une methode qui indique si user authentifier
+  public isAuthenticated () {
+    return this.authenticatedUser!=undefined
+  }
+}
